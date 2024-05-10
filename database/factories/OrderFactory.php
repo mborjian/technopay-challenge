cd2f2e85656f30d0cef8a0ea8982750d351bb4b5
Mehdi <mim.borjian@gmail.com>
2024-05-10 09:10:28 +0330

    Create OrderFactory.php
    create Order factory to set the fakers of order's fields.

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'customer_id' => User::factory(),
+            'status' => fake()->randomElement(['pending', 'completed', 'cancelled']),
+            'amount' => fake()->numberBetween(100, 1000000)
+        ];
+    }
+}
