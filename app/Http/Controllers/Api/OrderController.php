deaa9db6e96c401031eaba68868e8a59298893f3
Mehdi <mim.borjian@gmail.com>
2024-05-10 08:55:56 +0330

    Create OrderController.php
    create controller for api and add a filter func for filtering and showing the orders in api handler

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Exceptions\ApiException;
+use App\Http\Controllers\Controller;
+use App\Repositories\OrderRepository;
+use App\Specifications\OrderSpecifications\CustomerMobileSpecification;
+use App\Specifications\OrderSpecifications\CustomerNationalCodeSpecification;
+use App\Specifications\OrderSpecifications\OrderAmountRangeSpecification;
+use App\Specifications\OrderSpecifications\OrderStatusSpecification;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class OrderController extends Controller
+{
+    protected OrderRepository $orderRepository;
+
+    public function __construct(OrderRepository $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    /**
+     * Function of filtering orders based on incoming request parameters
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ApiException
+     */
+    public function filter(Request $request): JsonResponse
+    {
+        try {
+            // For test ApiException
+            // $a = 1 / 0;
+
+            $specs = [];
+
+            if ($request->has('status')) {
+                $specs[] = new OrderStatusSpecification($request->status);
+            }
+            if ($request->has('national_code')) {
+                $specs[] = new CustomerNationalCodeSpecification($request->national_code);
+            }
+            if ($request->has('mobile_number')) {
+                $specs[] = new CustomerMobileSpecification($request->mobile_number);
+            }
+            if ($request->has('min_amount') || $request->has('max_amount')) {
+                $specs[] = new OrderAmountRangeSpecification($request->min_amount, $request->max_amount);
+            }
+
+            $orders = $this->orderRepository->findBySpecs($specs);
+
+            return response()->json($orders);
+
+        } catch (Throwable $th) {
+            Log::error($th->getMessage());
+        }
+    }
+}
