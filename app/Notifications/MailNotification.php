85ab3a4b272cc97ee89eea89447fc767616cb551
Mehdi <mim.borjian@gmail.com>
2024-05-14 01:56:39 +0330

    Create single responsibility class 4 MailNotification

---
+++ app/Notifications/MailNotification.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class MailNotification extends Notification
+{
+    use Queueable;
+
+    protected string $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Api Error Notification')
+            ->line('An error occurred (Email).')
+            ->line('Error Details: ' . $this->message);
+    }
+}
