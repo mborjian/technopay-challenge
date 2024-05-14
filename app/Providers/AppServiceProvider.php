32197b0d21681d9e8e59a57303a261db61679624
Mehdi <mim.borjian@gmail.com>
2024-05-14 12:05:38 +0330

    bind OrderFilter in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,11 @@ namespace App\Providers;
 use App\Interfaces\NotificationSender;
 use App\Services\MailNotificationSender;
 use App\Services\SmsNotificationSender;
+use App\Strategies\OrderFilter;
+use App\Strategies\OrderStrategies\CustomerMobileFilter;
+use App\Strategies\OrderStrategies\CustomerNationalCodeFilter;
+use App\Strategies\OrderStrategies\OrderAmountRangeFilter;
+use App\Strategies\OrderStrategies\OrderStatusFilter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
                 new MailNotificationSender(),
                 new SmsNotificationSender(),
             ];
+        });
+
+        $this->app->bind(OrderFilter::class, function ($app) {
+            return new OrderFilter([
+                new OrderStatusFilter(),
+                new CustomerNationalCodeFilter(),
+                new CustomerMobileFilter(),
+                new OrderAmountRangeFilter()
+            ]);
         });
     }
 
